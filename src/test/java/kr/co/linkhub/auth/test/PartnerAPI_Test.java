c47342bd947cc1aa2f211cab7ea0fb4befb86233
john <john@linkhub.co.kr>
2023-02-28 11:38:56 +0900

    테스트 코드.

--- src/test/java/kr/co/linkhub/auth/test/PartnerAPI_Test.java
+++ src/test/java/kr/co/linkhub/auth/test/PartnerAPI_Test.java
@@ -11,19 +11,19 @@ import kr.co.linkhub.auth.TokenBuilder;
 
 public class PartnerAPI_Test {
 	
-	private final String LinkID = "LINKHUB1";
-    private final String SecretKey = "CAkTApmtepOsPvKxCFhvto0eniDrCV1SvrmezBVmXRY=";
+	private final String LinkID = "TESTER";
+    private final String SecretKey = "SwWxqU+0TErBXy/9TVjIPEnI0VTUMMSQZtJf3Ed8q3I=";
     
 	@Test
     public void PartnerMemberPointList() throws LinkhubException {
 		
 		TokenBuilder tokenBuilder = 
 				TokenBuilder.newInstance(LinkID, SecretKey) // LinkID, SecretKey는 발급받은 인증정보로 기재
-				.ServiceID("POPBILL_STAGE") // 운용환경은 "POPBILL", 테스트환경은 "POPBILL_TEST" 기재
+				.ServiceID("POPBILL") // 운용환경은 "POPBILL", 테스트환경은 "POPBILL_TEST" 기재
 				.addScope("partner") 		// 회원목록을 응답받기위해 "member"아닌 "partner" 기재
                 .addScope("110");			// 전자세금계산서 아이템코드 "110" 기재
 		
-		tokenBuilder.setServiceURL("https://dev-auth.linkhub.kr");
+		
 		
 		Token token = tokenBuilder.build();
 		
@@ -36,10 +36,19 @@ public class PartnerAPI_Test {
 				"6798700433","2198110402","4108600477","4091012658","1341986415","1231212312","2222222222","1111111111",
 				"1212062413","2309823538","1318607714","4040683485","6210450455","2102360093","1234567890","1208795568",
 				"3852012399","2258110770","3071033094","6473300302","3062772165","3164900035","1078628206","1023958230",
-				"2309582351","4364364331","2030273881","2309482392","2305982300","2352345320","3029482305","2309580921"};
+				"2309582351","4364364331","2030273881","2309482392","2305982300","2352345320","3029482305","2309580921","000-00-00900	"};
 		
-		// 회원 포인트 목록 조회 함수 호출.
-		MemberPointInfo[] pointInfo = tokenBuilder.listMemberPointInfo(token.getSession_token(), MemberCorpNums);
+		MemberPointInfo[] pointInfo = null;
+		
+		try {
+			// 회원 포인트 목록 조회 함수 호출.
+			pointInfo = tokenBuilder.listMemberPointInfo(token.getSession_token(), MemberCorpNums);
+		} catch (LinkhubException e) {
+			System.out.println(e.getCode());
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		
 		
 		System.out.println(pointInfo.length);
 		
